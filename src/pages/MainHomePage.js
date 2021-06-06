b5b39d510d97154dc2861f0309dc9b2753ef5f60
jslee7420 <jeongsoo.lee7420@gmail.com>
2021-06-06 22:20:42 +0900

    UPDATE home page& event

--- src/pages/MainHomePage.js
+++ src/pages/MainHomePage.js
@@ -5,6 +5,7 @@ import boardImage from '../discussion.png';
 import mapImage from '../Koedijkslanden.png';
 import './MainHomePage.css';
 import {useState} from 'react';
+import FarmGroup from '../components/farmGroup';
 
 
 function MainHomePage() {
@@ -13,7 +14,11 @@ function MainHomePage() {
     { groupName: 'FIGTING', crop: 'Tomato', date: '2021/04/01~' },
     { groupName: 'MUYAHO', crop: 'Strawberry', date: '2021/06/01~' },
   ]);
-  
+
+  const renderFgroup = fgroups.map(group=>{
+    return(<FarmGroup group={group}/>)
+  })
+
   return (
     <main>
       <div className="mainIntro">
@@ -30,15 +35,12 @@ function MainHomePage() {
 
       <div className="farmingGroups">
         <h2>
-          Currently there are {fgroups.length} farming groups in Urban green
+          Currently there are {fgroups.length} active farming groups in Urban green
         </h2>
-        <div className="fGroups">
-          <div className="fGroup">
-            <div className="groupName">{fgroups[0].groupName}</div>
-            <div className="crop">{fgroups[0].crop}</div>
-            <div className="date">{fgroups[0].date}</div>
-          </div>
+        <div className="activeGroups">
+          {renderFgroup}
         </div>
+        
       </div>
 
       <div className="functionIntro">
